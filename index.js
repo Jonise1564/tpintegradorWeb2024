5e6fbc36f2e4f0dec0dc9bd231d1f51a9b701fd0
Jonathan Gabriel Garay <103967649+Jonise1564@users.noreply.github.com>
2024-09-22 20:42:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const translate = require('node-google-translate-skidz');
+
+// Middleware para servir contenido estático
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Iniciar el servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+// Ruta para traducir texto
+app.get('/translate', (req, res) => {
+    const { text, targetLang } = req.body;
+
+    translate({
+        text: text,
+        source: 'en', // Idioma de origen (Inglés)
+        target: targetLang, // Idioma de destino (Español)
+    }, (result) => {
+        if (result && result.translation) {
+            res.json({ translatedText: result.translation });
+        } else {
+            res.status(500).json({ error: 'Error al traducir el texto' });
+        }
+    });
+});
