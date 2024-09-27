4c9ee094f5e21fdf5b81ec3546ae73a70cf659e2
Jonathan Gabriel Garay <103967649+Jonise1564@users.noreply.github.com>
2024-09-27 01:00:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,32 +4,25 @@ const translate = require('node-google-translate-skidz'); // Importa el paquete 
 const app = express();
 const port = 3000;
 
-app.use(express.json()); // Middleware para parsear JSON
+app.use(express.json());// Middleware para analizar el cuerpo de la solicitud JSON
 
-// Ruta para traducir propiedades de las cards
-app.post('/translate-card', async (req, res) => {
-    const { title, culture, dynasty } = req.body;
-    console.log(title);
-    console.log(title, culture, dynasty);
-    
-    try {
-        const translatedTitle = await translate({ text: title, source: 'en', target: 'es' });
-        console.log('Translated Title Response:', translatedTitle); // Imprime la respuesta completa para verificar su estructura
-        const translatedCulture = await translate({ text: culture, source: 'en', target: 'es' });
-        console.log('Translated Culture Response:', translatedCulture); // Imprime la respuesta completa para verificar su estructura
-        const translatedDynasty = await translate({ text: dynasty, source: 'en', target: 'es' });
-        console.log('Translated Dynasty Response:', translatedDynasty); // Imprime la respuesta completa para verificar su estructura
-
-        res.json({
-            translatedTitle: translatedTitle.translation, // Verificar que 'translation' es la propiedad correcta
-            translatedCulture: translatedCulture.translation,
-            translatedDynasty: translatedDynasty.translation
-        });
-    } catch (error) {
-        console.error('Error al traducir las propiedades de las cards:', error);
-        res.status(500).json({ error: 'Error al traducir las propiedades de las cards' });
-    }
+// Ruta para traducir texto
+app.post('/translate', (req, res) => {
+    const { text, targetLang } = req.body;
+    console.log(text);
+    translate({
+        text: text,
+        source: 'en', // Idioma de origen (Inglés)
+        target: targetLang, // Idioma de destino (Español)
+    }, (result) => {
+        if (result && result.translation) {
+            res.json({ translatedText: result.translation });
+        } else {
+            res.status(500).json({ error: 'Error al traducir el texto' });
+        }
+    });
 });
+
 
 // Servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
